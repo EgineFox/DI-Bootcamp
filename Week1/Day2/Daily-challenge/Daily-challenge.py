3e18c4be1034c54cc592ae03944de3f8028bb2c2
Ekaterina Morzhakova <ninorichi.israel@gmail.com>
2025-07-22 19:10:26 +0300

    Update Daily-challenge.py

--- Week1/Day2/Daily-challenge/Daily-challenge.py
+++ Week1/Day2/Daily-challenge/Daily-challenge.py
@@ -1,6 +1,6 @@
 # Ask for User Input:
 string = input("Enter a string exactly 10 characters long: ")
-index = 0
+index = ''
 # Check the Length of the String:
 if len(string) < 10:
     print("String not long enough")
@@ -10,8 +10,9 @@ elif len(string) == 10:
     print("Perfect string"), 
     # Print the First and Last Characters:
     print(string[0], string[-1]), 
-    for index in range(0, 9):
-        if index <= 10:
-            print(string[index])
-            index +=1
+    char = string[0]
+    for index in string:
+            index += char
+            print(string)
+          
           
