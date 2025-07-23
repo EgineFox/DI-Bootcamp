b47a2e6e2dfea5481ce65f1d81b4023f3ce1c820
Ekaterina Morzhakova <ninorichi.israel@gmail.com>
2025-07-23 21:41:04 +0300

    Create list.py

---
+++ Week1/Day2/Day3/list.py
@@ -0,0 +1,19 @@
+user_name = 'Ekaterina'
+email = 'ninori.chiisrael@gmail.com'
+user_age = 30
+is_online = True    
+
+user_info = [user_name, user_age, email, is_online]
+print(len(user_info))
+
+some_list = list('Item1')
+other_list = ['Item4', 'Item5', 'Item6']
+
+print(some_list)
+print(other_list)   
+
+empty_list = []
+
+print(user_info[1])
+
+print(user_info[0:2])
