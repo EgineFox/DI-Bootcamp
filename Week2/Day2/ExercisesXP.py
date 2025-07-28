c0c2db6d9bd55ef2515c2ea9e5caccaa642e7a7d
Ekaterina Morzhakova <ninorichi.israel@gmail.com>
2025-07-28 18:34:55 +0300

    Create ExercisesXP.py

---
+++ Week2/Day2/ExercisesXP.py
@@ -0,0 +1,27 @@
+# # Exercise 1: What Are You Learning?
+# # Create a function that pint my message
+# def display_message():
+#     '''Print message what about I learn'''
+#     print('Now I learn functions in the Python')
+
+# display_message()
+
+# #  Exercise 2: What’s Your Favorite Book?
+# def favorite_book(title):
+# '''Create a function that displays a message about a favorite book.'''
+#     print(f"One of my favorite books is {title}")
+
+# favorite_book("Anna Korenina by Tolstoy")
+
+# # Exercise 3: Some Geography
+# # Create a function that describes a city and its country.
+# def describe_city(city, country = "Unknown"):
+#     print(f"The {city} is in the {country}")
+
+# city = "Moscow"
+# country = "Russia"
+# describe_city(city,country)
+
+# Exercise 4: Random
+
+
