af3ed8bf6342fd0d0cc9b38cf1cd789aed9d0b9d
Ekaterina Morzhakova <ninorichi.israel@gmail.com>
2025-07-28 18:34:52 +0300

    Update Functions.py

--- Week2/Day1/Functions.py
+++ Week2/Day1/Functions.py
@@ -1,7 +1,23 @@
-# Ex1
-def my_func():
-    '''Print string in console'''
-    print('Hello there!')
+# # Ex1
+# def my_func():
+#     '''Print string in console'''
+#     print('Hello there!')
 
 
-my_func()
+# my_func()
+
+# # passing arguments to the function
+
+# def my_func_adv(lang):
+#     '''Prints a greeating on the languages'''
+#     if lang == 'PT':
+#         print('Ola!')
+#     elif lang == 'ES':
+#         print('Hola')
+
+#     else:
+#         print('Unknown language')
+
+
+# my_func_adv('PT')
+
