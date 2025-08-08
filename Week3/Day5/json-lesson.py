7e0577db494d2b34740ae9645bba6a22db85f980
Ekaterina Morzhakova <ninorichi.israel@gmail.com>
2025-08-08 17:06:52 +0300

    Create json-lesson.py

---
+++ Week3/Day5/json-lesson.py
@@ -0,0 +1,28 @@
+import json
+import os
+
+
+# dir_path = os.path.dirname(os.path.realpath(__file__))
+
+# my_family = {
+#     "parents":['Beth', 'Jerry'],
+#     "children":['Summer', 'Morty']
+# }
+# # # convert dict to json
+# # with open(f"{dir_path}/family.json",'w') as f:
+# #     json.dump(my_family, f)
+# # # convert a dict to a json string
+# json_my_family_string = json.dumps(my_family)
+# # print(json_my_family_string)
+
+# # # convert from a json to a dict 
+# # with open(f"{dir_path}/family.json",'r') as f:
+# #     my_family2 = json.load(f)
+
+# # print(my_family2)
+
+# # convert from a json to a dict
+# parsed_family = json.loads(json_my_family_string)
+# print(parsed_family)
+
+# Ex
