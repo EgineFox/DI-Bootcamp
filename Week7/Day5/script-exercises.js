8b6bfabeef0c3441258607bb718ff3a11af6cfcd
Ekaterina Morzhakova <ninorichi.israel@gmail.com>
2025-09-06 18:59:22 +0300

    Create script-exercises.js

---
+++ Week7/Day5/script-exercises.js
@@ -0,0 +1,34 @@
+//Exercise 1
+/*
+function myInfo(myAge) {
+    let momsAge = myAge * 2;
+    let dadAge = momsAge*1.2;
+    console.log(`My mom is ${momsAge} years old, and my dad is ${dadAge}`);
+
+}
+
+myInfo(32);
+*/
+
+//Exercise 2
+/*
+function myInfo(myAge) {
+    let momsAge = myAge * 2;
+    return momsAge;
+}
+myInfo(32);
+*/
+
+//Exercise 3
+/*
+const calc = (a, b, operator) =>
+  operator === '+' ? a + b :
+  operator === '-' ? a - b :
+  operator === '*' ? a * b :
+  operator === '/' ? (b !== 0 ? a / b : "Cannot divide by zero") :
+  "Invalid operator";
+
+console.log(calc(5, 10, "+"));
+*/
+
+
