20766f8dd739f43fa708da7752c8d66fc4b2edb3
Ekaterina Morzhakova <ninorichi.israel@gmail.com>
2025-09-16 16:46:58 +0300

    Create Exercise 3.js

---
+++ Week9/Day2/Exercises-XP/Exercise 3.js
@@ -0,0 +1,12 @@
+const isString = value => typeof value === 'string';
+
+
+
+console.log(isString('hello'));        // true
+console.log(isString([1, 2, 4, 0]));   // false
+
+
+
+/*Explanation:
+- typeof value === 'string' checks if the type of the input is exactly a string.
+- Arrays, numbers, objects, etc., will return false. */
