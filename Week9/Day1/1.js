38a1ce147814a529edd5f5165bf93660506c3c47
Ekaterina Morzhakova <ninorichi.israel@gmail.com>
2025-09-16 16:21:25 +0300

    Create 1.js

---
+++ Week9/Day1/1.js
@@ -0,0 +1,44 @@
+//Advanced Javascript Functions - Exercise 1
+/*
+function CheckYear(year) {
+  return year < 2000? "You are in the 21st century" : "You live in the Middle Age"
+};
+
+CheckYear(2025);
+
+
+function CheckY(y) {
+    if (y<2000) {
+        return "You are in the 21st century";
+    } else {
+        return "You live in the Middle Age";
+    }
+};
+
+CheckY(1990);
+
+CheckYe = ye => ye < 2000? "You are in the 21st century" : "You live in the Middle Age"
+};
+
+CheckYe(2025);
+
+
+
+// Arrow function - - Exercise Calculation
+
+const calculator = (operator, a,b) => operator === "+"? a+b : operator === "*"? a*b : operator === "-"? a-b : operator === "/"? a/b : "Invalid operator";
+
+calculator ('+', 5, 3);
+calculator ('-', 5, 3);
+calculator ('*', 5, 3);
+calculator ('/', 5, 3);
+calculator ('%', 5, 3);
+
+const calculator2 = (operator, a,b) => operator === "+"? a+b : operator === "*"? a*b : operator === "-"? a-b : b===0? "Try again! It\'s not in math rules!" : operator === "/"? a/b :  "Invalid operator";
+
+calculator2 ('+', 5, 3);
+calculator2 ('-', 5, 3);
+calculator2 ('*', 5, 3);
+calculator2 ('/', 5, 3);
+calculator2 ('%', 5, 3);
+*/
