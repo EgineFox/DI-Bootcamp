4aa6dfd7da6eab59550bc5ae0e6433a39921c048
Ekaterina Morzhakova <ninorichi.israel@gmail.com>
2025-09-16 16:46:55 +0300

    Create Exercise 2.js

---
+++ Week9/Day2/Exercises-XP/Exercise 2.js
@@ -0,0 +1,14 @@
+// Transform winBattle to an arrow function
+const winBattle = () => true;
+
+
+
+// Use a ternary operator to assign experiencePoints
+const experiencePoints = winBattle() ? 10 : 1;
+
+
+
+// Log the result
+console.log(experiencePoints); // Output: 10
+
+
