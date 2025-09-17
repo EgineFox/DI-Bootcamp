2df55af5083a49d174af227c6449dc5c537b738b
Ekaterina Morzhakova <ninorichi.israel@gmail.com>
2025-09-17 21:56:05 +0300

    Update Exercise 3.js

--- Week9/Day2/Exercises-XP/Exercise 3.js
+++ Week9/Day2/Exercises-XP/Exercise 3.js
@@ -3,7 +3,7 @@ const isString = value => typeof value === 'string';
 
 
 console.log(isString('hello'));        // true
-console.log(isString([1, 2, 4, 0]));   // false
+console.log(isString(5));   // false
 
 
 
