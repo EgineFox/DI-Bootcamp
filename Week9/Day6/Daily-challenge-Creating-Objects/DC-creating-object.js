6a100b2d12187e93d756bc6a2dbe7e2030f4e80a
Ekaterina Morzhakova <ninorichi.israel@gmail.com>
2025-09-18 20:17:56 +0300

    Create DC-creating-object.js

---
+++ Week9/Day6/Daily-challenge-Creating-Objects/DC-creating-object.js
@@ -0,0 +1,35 @@
+class Video {
+    constructor (title, uploader, time) {
+       this.title = title,
+       this.uploader = uploader,
+       this.time  = time;
+    }
+    watch() {
+        alert(` ${this.uploader} watched all ${this.time} of ${this.title}!`) ;
+    }
+}
+
+
+let video1 = new Video("Very fanny video", "Michael", 60);
+video1.watch(); 
+
+let video2 = new Video("Educational video", "Anna", 70);
+
+//Bonus
+
+let arrayVideo = [
+  { title: '1', uploader: 'Stas', time: 20 },
+  { title: '2', uploader: 'Inna', time: 30 },
+  { title: '3', uploader: 'Person', time: 40 },
+  { title: '4', uploader: 'Human', time: 50 },
+  { title: '5', uploader: 'Rabbit', time: 60 }
+];
+
+// Looping to Create Video Instances
+
+arrayVideo.forEach(data => {
+    const video = new Video( data.title, data.uploader, data.time);
+    video.watch();
+
+});
+    
