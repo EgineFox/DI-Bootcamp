cb4c448e681c464d7abe7edb64a5e3d1038c9833
Ekaterina Morzhakova <ninorichi.israel@gmail.com>
2025-12-09 21:53:55 +0200

    Remove App.jsx and selectors.js files
    Deleted App.jsx and selectors.js from the Daily-Challenge-Planner-Application/Repo directory. These files are no longer needed, possibly due to refactoring or restructuring of the application.

--- Week18/Day6/Daily-Challenge-Planner-Application/Repo/selectors.js
+++
@@ -1,6 +0,0 @@
-export const selectSelectedDate = (state) => state.selectedDate;
-
-export const selectTasksForSelectedDay = (state) => {
-  const key = state.selectedDate;
-  return state.tasksByDay[key] || [];
-};
